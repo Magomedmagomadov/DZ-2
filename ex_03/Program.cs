b27cbc5bd1cd5f4dfe32ed4cfaee55926cc413ba
Magomed Magomadov <magomed.magomadov_124@mail.ru>
2022-09-27 21:27:07 +0300

    В папке ех_03 13 задачка

--- ex_03/Program.cs
+++ ex_03/Program.cs
@@ -11,4 +11,11 @@
 Console.Clear();
 Console.Write("Введите число:");
 int n = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine(n % 20);
+if (n < 100)
+Console.WriteLine("Третьей цифры нет");
+else 
+{
+    while (n > 1000)
+        n = n / 10;
+    Console.WriteLine($"Третья цифра числа: {n % 10}");
+}
